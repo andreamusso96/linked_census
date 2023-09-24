513264067468d09ea31b43f3847cd65b581137ac
andreamusso96 <andreamusso96@gmail.com>
2023-09-24 15:21:43 +0200

    Industrial composition

--- src/linked_census/__init__.py
+++ src/linked_census/__init__.py
@@ -1,3 +1,3 @@
-from . interface import get_intercity_migrations, get_city_population
+from . interface import get_intercity_migrations, get_city_population, get_city_industrial_composition
 from . data import data
 from . enums import CensusYear, Industry, PlaceClusterLevel
